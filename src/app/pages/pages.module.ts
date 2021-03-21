9032731a0d57005e6e76271e91617ad9330d1cf7
Diego Vizhnay <diego@vzhny.com>
2021-03-21 14:23:45 -0400

    Added pages module

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeModule } from './home/home.module';
+import { DailiesModule } from './dailies/dailies.module';
+import { BossesModule } from './bosses/bosses.module';
+import { GuidesModule } from './guides/guides.module';
+import { SettingsComponent } from './settings/settings.component';
+import { SettingsModule } from './settings/settings.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HomeModule,
+    DailiesModule,
+    BossesModule,
+    GuidesModule,
+    SettingsModule,
+  ],
+})
+export class PagesModule {}
