d86db0e06778682709255eef874141fd4fd98d9a
Diego Vizhnay <diego@vzhny.com>
2021-03-21 14:30:00 -0400

    Added app module and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { routes } from './app.routes';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/app.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+import { BossesComponent } from './pages/bosses/bosses.component';
+import { DailiesComponent } from './pages/dailies/dailies.component';
+import { GuidesComponent } from './pages/guides/guides.component';
+import { HomeComponent } from './pages/home/home.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'dailies',
+    loadChildren: () =>
+      import('./pages/dailies/dailies.module').then((m) => m.DailiesModule),
+  },
+  {
+    path: 'bosses',
+    loadChildren: () =>
+      import('./pages/bosses/bosses.module').then((m) => m.BossesModule),
+  },
+  {
+    path: 'guides',
+    loadChildren: () =>
+      import('./pages/guides/guides.module').then((m) => m.GuidesModule),
+  },
+  {
+    path: 'settings',
+    loadChildren: () =>
+      import('./pages/settings/settings.module').then((m) => m.SettingsModule),
+  },
+];
