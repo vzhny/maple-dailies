985b7e144cec26bc4d7742d3dec6067290706551
Diego Vizhnay <diego@vzhny.com>
2021-04-14 17:57:03 -0400

    Updated home page copy

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { FrameworkModule } from 'src/app/framework/framework.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, FrameworkModule],
 })
 export class HomeModule {}
