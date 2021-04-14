7a8f8dfb7b0535f7f9b76c82f6ad80e5b5ec2bf6
Diego Vizhnay <diego@vzhny.com>
2021-04-14 17:57:13 -0400

    Added settings page placeholder

--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { SettingsRoutingModule } from './settings-routing.module';
+import { FrameworkModule } from 'src/app/framework/framework.module';
+import { SettingsComponent } from './settings.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, SettingsRoutingModule],
+  declarations: [SettingsComponent],
+  imports: [CommonModule, SettingsRoutingModule, FrameworkModule],
 })
 export class SettingsModule {}
