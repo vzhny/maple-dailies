19142cac8bc5469fc9a51a9744b315a6e1ca7abe
Diego Vizhnay <diego@vzhny.com>
2021-04-14 17:54:02 -0400

    Added WIP message to bosses page

--- src/app/pages/bosses/bosses.module.ts
+++ src/app/pages/bosses/bosses.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BossesRoutingModule } from './bosses-routing.module';
 import { BossesComponent } from './bosses.component';
+import { FrameworkModule } from 'src/app/framework/framework.module';
 
 @NgModule({
   declarations: [BossesComponent],
-  imports: [CommonModule, BossesRoutingModule],
+  imports: [CommonModule, BossesRoutingModule, FrameworkModule],
 })
 export class BossesModule {}
