cadc1908bb40cee8bcdf0be5b9985a34e348a776
Diego Vizhnay <diego@vzhny.com>
2021-04-16 21:49:28 -0400

    Added dark mode related component logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { LocalStorageService } from './utils/local-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'maple-dailies';
+  darkModeEnabledKey = 'darkModeEnabled';
+
+  constructor(private localStorage: LocalStorageService) {}
+
+  ngOnInit(): void {
+    this.localStorage
+      .watch(this.darkModeEnabledKey)
+      .subscribe((value: string) => this.setDarkModeOnDocument(value));
+
+    const darkModeEnabledValue = this.localStorage.get(this.darkModeEnabledKey);
+
+    if (darkModeEnabledValue === null) {
+      this.localStorage.set(this.darkModeEnabledKey, 'true');
+      this.setDarkModeOnDocument('true');
+    } else {
+      this.setDarkModeOnDocument(darkModeEnabledValue);
+    }
+  }
+
+  setDarkModeOnDocument(value: string) {
+    const darkModeEnabled = value === 'true';
+
+    const htmlElement = document.querySelector('html');
+
+    if (htmlElement) {
+      htmlElement.className = darkModeEnabled ? 'dark' : 'light';
+    }
+  }
 }
