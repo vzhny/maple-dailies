ffab83b8f4045f042f238c4338e419c611e543ba
Diego Vizhnay <diego@vzhny.com>
2021-06-01 00:12:49 -0400

    Updated boss checklist action buttons to use tippy tooltips; also includes dashboard specific updates

--- src/app/pages/bosses/bosses.module.ts
+++ src/app/pages/bosses/bosses.module.ts
@@ -7,9 +7,11 @@ import { FrameworkModule } from 'src/app/framework/framework.module';
 import { BossesChecklistComponent } from './components/bosses-checklist/bosses-checklist.component';
 import { CustomPipesModule } from 'src/app/utils/pipes/custom-pipes.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TippyModule } from '@ngneat/helipopper';
 
 @NgModule({
   declarations: [BossesComponent, BossesChecklistComponent],
-  imports: [CommonModule, BossesRoutingModule, FrameworkModule, FontAwesomeModule, CustomPipesModule],
+  imports: [CommonModule, BossesRoutingModule, FrameworkModule, FontAwesomeModule, TippyModule, CustomPipesModule],
+  exports: [BossesChecklistComponent],
 })
 export class BossesModule {}
