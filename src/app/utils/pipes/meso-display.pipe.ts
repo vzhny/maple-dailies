16a5db3ae8c428d317cb611af476e7d57ad3f6a6
Diego Vizhnay <diego@vzhny.com>
2021-06-18 22:54:19 -0400

    Updated import

--- src/app/utils/pipes/meso-display.pipe.ts
+++ src/app/utils/pipes/meso-display.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DisplayService } from 'src/app/utils/display.service';
+import { DisplayService } from 'src/app/utils/services/display.service';
 
 @Pipe({
   name: 'mesos',
