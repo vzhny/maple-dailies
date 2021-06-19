f744d501ab126f27d3dbb2ba0aed672f3e8039e2
Diego Vizhnay <diego@vzhny.com>
2021-06-18 22:46:15 -0400

    Updated table template, added pagination component and related logic; removed set data logic

--- src/app/framework/table/table.module.ts
+++ src/app/framework/table/table.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table.component';
 import { FormModule } from '../form/form.module';
+import { PaginationModule } from '../pagination/pagination.module';
 
 @NgModule({
   declarations: [TableComponent],
-  imports: [CommonModule, FormModule],
+  imports: [CommonModule, FormModule, PaginationModule],
   exports: [TableComponent],
 })
 export class TableModule {}
