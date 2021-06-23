ae776f490a8496c10d241a29f33ea02cb1da8ded
Diego Vizhnay <diego@vzhny.com>
2021-06-23 19:31:04 -0400

    Updated home component with copy

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { FrameworkModule } from 'src/app/framework/framework.module';
+import { HomeDetailsHeroSectionComponent } from './home-details-hero-section/home-details-hero-section.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeDetailsHeroSectionComponent],
   imports: [CommonModule, HomeRoutingModule, FrameworkModule],
 })
 export class HomeModule {}
