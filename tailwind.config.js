c9e9f969013e591562aea6d2990082d374e21d18
Diego Vizhnay <diego@vzhny.com>
2021-07-04 02:39:21 -0400

    Added daisyui config and dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,7 +45,40 @@ module.exports = {
       },
     },
   },
-  plugins: [forms],
+  plugins: [forms, typography, daisyui],
+  daisyui: {
+    themes: [
+      {
+        'maple-dailies-dark': {
+          primary: '#6EE7B7',
+          'primary-focus': '#10B981',
+          'primary-content': '#171717',
+
+          secondary: '#4C566A' /* Secondary color */,
+          'secondary-focus': '#454D5F' /* Secondary color - focused */,
+          'secondary-content': '#F9FAFB' /* Foreground content color to use on secondary color */,
+
+          accent: '#a991f7' /* Accent color */,
+          'accent-focus': '#8462f4' /* Accent color - focused */,
+          'accent-content': '#F9FAFB' /* Foreground content color to use on accent color */,
+
+          neutral: '#3d4451' /* Neutral color */,
+          'neutral-focus': '#2a2e37' /* Neutral color - focused */,
+          'neutral-content': '#F9FAFB' /* Foreground content color to use on neutral color */,
+
+          'base-100': '#4B5563' /* Base color of page, used for blank backgrounds */,
+          'base-200': '#46505D' /* Base color, a little darker */,
+          'base-300': '#3D4651' /* Base color, even more darker */,
+          'base-content': '#F9FAFB' /* Foreground content color to use on base color */,
+
+          info: '#2094f3' /* Info */,
+          success: '#009485' /* Success */,
+          warning: '#ff9900' /* Warning */,
+          error: '#ff5724' /* Error */,
+        },
+      },
+    ],
+  },
   variants: {
     extend: {
       backgroundColor: ['checked', 'disabled'],
