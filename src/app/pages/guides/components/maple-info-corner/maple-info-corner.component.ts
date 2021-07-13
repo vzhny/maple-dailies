31d39a08e15191fb1bce63c2860bd31d1fdda2fd
Diego Vizhnay <diego@vzhny.com>
2021-07-12 20:59:55 -0400

    Added maple info corner guide component

---
+++ src/app/pages/guides/components/maple-info-corner/maple-info-corner.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maple-info-corner',
+  templateUrl: './maple-info-corner.component.html',
+})
+export class MapleInfoCornerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
